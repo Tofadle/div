73a6f158c9fa5b640622002712daedae2437e8c8
tormodder <tormodfjeldlie@gmail.com>
2022-03-14 23:17:46 +0100

    fix existing directory problem. clean up code.

--- sorter/sorter.py
+++ sorter/sorter.py
@@ -9,7 +9,10 @@ os.chdir(dname)
 
 
 def args():
-    help_message = "-file, -f sorter.txt to open from file\ntext format:\n <source> <category>:<path> <category>:<path>"
+    """
+    Resolves sys.argvs with the format: \n <source> <category>:<destination path> <category>:<destination path>
+    """
+    help_message = "-file, -f sorter.txt to open from file\ntext format:\n <source> <category>:<destination path> <category>:<destination path>"
     arglist = []
 
     if sys.argv[1] == "-f" or "-file":
@@ -35,21 +38,28 @@ def args():
 #TODO: Figure out a way to display progress when moving files
 def main(source, cat1, pat1, cat2, pat2):    
     os.chdir(source)
+    print("Sorting from {source}.\n")
     for item in os.listdir():
-            sort = input(f"Sorting from {source}.\nis {item} a {cat1} or {cat2}?\n---> type: {cat1}/{cat1[0]} or {cat2}/{cat2[0]} to sort\n(Any key to skip)\n> ")
-            #sort in category 1
-            #TODO: Make sure shutil.Error: Destination path already exists doesn't happen
-            if sort == f"{cat1}" or sort == f"{cat1[0]}":
-                newpat1 = pat1 + "/" + item
-                print(f"moving {item}...")
-                shutil.move(item, newpat1)
-                #sort in category 2
-            elif sort == f"{cat2}" or sort == f"{cat2[0]}":
-                newpat2 = pat2 + "/" + item
-                print(f"moving {item}")
-                shutil.move(item, newpat2)
+            sort = input(f"is {item} a {cat1} or {cat2}?\n---> type: {cat1}/{cat1[0]} or {cat2}/{cat2[0]} to sort\n(Any key to skip)\n> ")
+            #sort in categories
+            if sort.startswith(cat1[0]):
+                newpat = pat1 + "/" + item
+            elif sort.startswith(cat2[0]):
+                newpat = pat2 + "/" + item
             else:
-                print(f"leaving {item}")
+                continue
+            if os.path.isdir(newpat):
+                print(f"{item} already exists at Destination. Overwrite? y/n")
+                overwrite = input("\n> ")
+                if overwrite.lower() == "y":
+                    shutil.rmtree(newpat, ignore_errors=True)
+                    shutil.move(item, newpat)
+                else:
+                    print(f"leaving {item}")        
+                    print(f"moving {item}...")
+            else:
+                shutil.move(item, newpat)
+
 
 if __name__ == "__main__":
     one, two, three, four, five = args()
