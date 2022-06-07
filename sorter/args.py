19e2f6564884d9225262bce0fd6af875aae1ca91
Tmod <tormodfjeldlie@gmail.com>
2022-06-07 12:56:01 +0200

    add args as own file

---
+++ sorter/args.py
@@ -0,0 +1,24 @@
+import sys
+
+def args(textfile):
+    """
+    Resolves sys.argvs with the format: \n <source> movies:<movie folder path> series:<series folder path>
+    """
+    help_message = "Needs to have sorter.txt in script directory\ntext format:\n <source> <category>:<destination path> <category>:<destination path>"
+    try:
+        with open(textfile, 'r') as f:
+            #TODO: use regex to test args in .txt file
+            args = f.readlines()
+            source, split1, split2 = args[0].split(" ")#maybe find a more elegant way to get rid of the space
+            cat1, pat1, cat2, pat2 = split1.split(":")[0], split1.split(":")[1], split2.split(":")[0], split2.split(":")[1]
+            if "\n" in pat2:
+                pat2 = pat2[:-1]
+            sorters = dict([(cat1, pat1), (cat2, pat2)])
+        return source, sorters
+    except FileNotFoundError:
+        print(help_message)
+    if sys.argv[1] == "-h" or "-help":
+        print(help_message)
+    else:
+        print(help_message)
+        quit()
--- sorter/sorter.py
+++ sorter/sorter.py
@@ -1,4 +1,4 @@
-import os, sys
+import os, sys, args
 from posixpath import abspath
 
 #Change working directory to script directory
@@ -6,29 +6,13 @@ abs = os.path.abspath(__file__)
 dname = os.path.dirname(abs)
 os.chdir(dname)
 
-def args():
-    """
-    Resolves sys.argvs with the format: \n <source> movies:<movie folder path> series:<series folder path>
-    """
-    help_message = "Needs to have sorter.txt in script directory\ntext format:\n <source> <category>:<destination path> <category>:<destination path>"
-    try:
-        with open("./sorter.txt", 'r') as f:
-            #TODO: use regex to test args in .txt file
-            args = f.readlines()
-            source, split1, split2 = args[0].split(" ")#maybe find a more elegant way to get rid of the space
-            cat1, pat1, cat2, pat2 = split1.split(":")[0], split1.split(":")[1], split2.split(":")[0], split2.split(":")[1]
-            if "\n" in pat2:
-                pat2 = pat2[:-1]
-            sorters = dict([(cat1, pat1), (cat2, pat2)])
-        return source, sorters
-    except FileNotFoundError:
-        print(help_message)
-    if sys.argv[1] == "-h" or "-help":
-        print(help_message)
-    else:
-        print(help_message)
-        quit()
 
-def main(:
+
+def main():
     pass
+    
+    #for root, dirs, file in os.walk()
+    #pass
 
+if __name__ == "__main__":
+    args.args("./sorter.txt")
