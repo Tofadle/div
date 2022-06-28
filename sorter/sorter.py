7c055829802763fbad5a4a463a54083a8888716b
Tmod <tormodfjeldlie@gmail.com>
2022-06-29 01:54:17 +0200

    error handling

--- sorter/sorter.py
+++ sorter/sorter.py
@@ -41,44 +41,40 @@ def movie(dirlist, filelist):
     elif len(video_list) >= 3 or len(dirlist) >= 2:#Assume it's a series with two or more seasons in a dir
         return False
 
-def maintain_series_dir(filelist, source, root):
-    """
-    Check if root=source, and if so move all of root
-    """
-   #nothing for now
-   #TODO: So I Simply want to maintain the 
-
 def main(*args):
     source = args[0][0]
     sorters = args[0][1]
 
     for root, dirs, file in os.walk(source):
-        if root == source:
-            #Check for video files in source and assume they are movies
-            for i in file:
-                if check_endings(i):
-                    #uncomment for test run:
-                    #shutil.move(f"{root}" + f"{file}", sorters['movies']) 
-                    logging.debug(f"{debug_format(source)}/{i} is being moved to {debug_format(sorters['movies'])}")
-                    shutil.move(root + "/" + i, sorters["movies"])
-        
-        
-        #TODO: Check other directories
-        else:
-            if not movie(dirs, file):
-                if len(dirs) >= 2:
-                    #move nested dir
-                    season_dir = root
-                    logging.debug(f"{debug_format(season_dir)} is being moved to {debug_format(sorters['series'])}")
-                    shutil.move(season_dir, sorters["series"])
-                else:
-                    #move single season dir
-                    logging.debug(f"{debug_format(root)} is being moved to {debug_format(sorters['series'])}")
-                    shutil.move(root, sorters["series"])
-            elif movie(dirs, file):
-                #move movie
-                logging.debug(f"{debug_format(root)} is being moved to {debug_format(sorters['movies'])}")
-                shutil.move(root, sorters['movies'])
+        try:
+            if root == source:
+                #Check for video files in source and assume they are movies
+                for i in file:
+                    if check_endings(i):
+                        #uncomment for test run:
+                        #shutil.move(f"{root}" + f"{file}", sorters['movies']) 
+                        logging.debug(f"{debug_format(source)}/{i} is being moved to {debug_format(sorters['movies'])}")
+                        shutil.move(root + "/" + i, sorters["movies"])
+
+
+            #TODO: Check other directories
+            else:
+                if not movie(dirs, file):
+                    if len(dirs) >= 2:
+                        #move nested dir
+                        season_dir = root
+                        logging.debug(f"{debug_format(season_dir)} is being moved to {debug_format(sorters['series'])}")
+                        shutil.move(season_dir, sorters["series"])
+                    else:
+                        #move single season dir
+                        logging.debug(f"{debug_format(root)} is being moved to {debug_format(sorters['series'])}")
+                        shutil.move(root, sorters["series"])
+                elif movie(dirs, file):
+                    #move movie
+                    logging.debug(f"{debug_format(root)} is being moved to {debug_format(sorters['movies'])}")
+                    shutil.move(root, sorters['movies'])
+        except shutil.Error as e:
+            logging.info(e)
 
 if __name__ == "__main__":
     main(args("./sorter.txt"))
